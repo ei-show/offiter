b42ec7da04d25e5f73063ccd7d03ea40045b4055
ei-show <offiter.ei.show@gmail.com>
2021-02-28 11:16:10 +0900

    tailwindのテスト

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <p className="bg-black text-white">test</p>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
