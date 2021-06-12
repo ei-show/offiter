151f19d153867cfe21c9d6bb38b983dd76d4734e
ei-show <offiter.ei.show@gmail.com>
2021-06-12 15:01:27 +0900

    全体的な色の変更

--- components/FooterNavItem.tsx
+++ components/FooterNavItem.tsx
@@ -5,7 +5,7 @@ export default function NavItem(props) {
   return (
     <li className="flex-1 text-center">
       <Link href="/">
-        <a className="block py-3 w-full bg-gray-300 leading-normal">
+        <a className="block py-3 w-full leading-normal">
           <FontAwesomeIcon icon={props.iconName} size="lg" fixedWidth />
           <p>{props.itemName}</p>
         </a>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import NavItem from './FooterNavItem'
 
 export default function Nav() {
   return (
-    <nav className="fixed bottom-0 left-0 md:hidden w-screen bg-white">
+    <nav className="fixed bottom-0 left-0 text-gray-800 md:hidden w-screen bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200">
       <ul className="flex">
         <NavItem itemName="Home" iconName={['fas', 'home']}/>
         <NavItem itemName="About" iconName={['fas', 'user']}/>
