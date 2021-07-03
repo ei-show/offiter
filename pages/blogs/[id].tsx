c29d85359e68e032b479f38daea307c75b57a375
ei-show <ei.show@gmail.com>
2021-07-03 23:25:35 +0900

    Apply next-seo

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { NextSeo } from 'next-seo'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import cheerio from 'cheerio';
 import hljs from 'highlight.js'
@@ -8,6 +9,7 @@ import 'highlight.js/styles/night-owl.css'
 import Layout from '@/components/Layout'
 import Date from '@/components/Date'
 import Style from '@/styles/blog.module.scss'
+import SEO from '@/lib/next-seo.config'
 
 export const getStaticPaths = async () => {
   const key: any = { headers: { 'X-API-KEY': process.env.API_KEY } }
@@ -46,45 +48,65 @@ export const getStaticProps = async context => {
 
 export default function Blog(props) {
   return (
-    <Layout blog={props.blog} blogs={props.blogs} tags={props.tags}>
-
-      <div className="flex items-center justify-between">
-        <h2 className="font-head text-xl text-gray-700 md:text-2xl">{props.blog.title}</h2>
-      </div>
-
-      <div className="mt-4 flex items-center justify-between">
-        <div className="flex flex-col">
-          <span className="text-xs font-light text-gray-600">
-            <FontAwesomeIcon icon="calendar-plus" fixedWidth />
-            {Date(props.blog.createdAt)}
-          </span>
-          <span className="text-xs font-light text-gray-600">
-            <FontAwesomeIcon icon="edit" fixedWidth />
-            {Date(props.blog.updatedAt)}
-          </span>
-        </div>
-
-        <div className="flex items-center justify-end">
-          {props.blog.tags.map(tag => (
-            <React.Fragment key={tag.id}>
-              <Link href="/[tag]" as={`/${tag.id}`}>
-                <a className="font-bold bg-gradient-to-r from-gray-50 via-white to-gray-50 text-blue-900 ml-2 p-2 rounded-lg shadow-md lg:shadow-none lg:transition lg:duration-300 lg:ease-in-out lg:transform lg:hover:-translate-y-1 lg:hover:shadow-md">
-                  <span>{tag.name}</span>
-                </a>
-              </Link>
-            </React.Fragment>
-          ))}
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center mt-4 border-8 border-gray-50 ">
-        <Image alt="" src={props.blog.image.url} width={props.blog.image.width} height={props.blog.image.height} />
-      </div>
-
-      <div
-        className={`${Style.blog} mt-4`}
-        dangerouslySetInnerHTML={{ __html: `${props.highlightedBody}` }}
+    <>
+      <NextSeo
+        {...SEO}
+        title={props.blog.title}
+        titleTemplate="%s | Offiter"
+        description={props.blog.description}
+        openGraph={{
+          url: `https://offiter.net/blogs/${props.blog.id}`,
+          title: props.blog.title,
+          description: props.blog.description,
+          images: [
+            {
+              url: props.blog.image.url,
+              height: props.blog.image.height,
+              width: props.blog.image.width,
+            },
+          ],
+        }}
       />
-    </Layout>
+      <Layout blog={props.blog} blogs={props.blogs} tags={props.tags}>
+
+        <div className="flex items-center justify-between">
+          <h2 className="font-head text-xl text-gray-700 md:text-2xl">{props.blog.title}</h2>
+        </div>
+
+        <div className="mt-4 flex items-center justify-between">
+          <div className="flex flex-col">
+            <span className="text-xs font-light text-gray-600">
+              <FontAwesomeIcon icon="calendar-plus" fixedWidth />
+              {Date(props.blog.createdAt)}
+            </span>
+            <span className="text-xs font-light text-gray-600">
+              <FontAwesomeIcon icon="edit" fixedWidth />
+              {Date(props.blog.updatedAt)}
+            </span>
+          </div>
+
+          <div className="flex items-center justify-end">
+            {props.blog.tags.map(tag => (
+              <React.Fragment key={tag.id}>
+                <Link href="/[tag]" as={`/${tag.id}`}>
+                  <a className="font-bold bg-gradient-to-r from-gray-50 via-white to-gray-50 text-blue-900 ml-2 p-2 rounded-lg shadow-md lg:shadow-none lg:transition lg:duration-300 lg:ease-in-out lg:transform lg:hover:-translate-y-1 lg:hover:shadow-md">
+                    <span>{tag.name}</span>
+                  </a>
+                </Link>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center mt-4 border-8 border-gray-50 ">
+          <Image alt="" src={props.blog.image.url} width={props.blog.image.width} height={props.blog.image.height} />
+        </div>
+
+        <div
+          className={`${Style.blog} mt-4`}
+          dangerouslySetInnerHTML={{ __html: `${props.highlightedBody}` }}
+        />
+      </Layout>
+    </>
   )
 }
