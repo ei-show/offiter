986b1f95885a0d3abb6f3da6c1763745ecf0af69
ei-show <offiter.ei.show@gmail.com>
2022-01-14 08:26:02 +0900

    ページネーションページの作成

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,14 +7,13 @@ type props = {
 
 export default function Pagination({ totalCount }: props): JSX.Element {
   const PER_PAGE: number = 10
-
   const range = (start: number, end: number) => [...Array(end - start + 1)].map((_, i) => start + i)
 
   return (
     <ul>
       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
         <li key={index}>
-          <Link href={ `/pages/${number}`}>
+          <Link href={ `/pages/blogs/${number}`}>
             <a>{number}</a>
           </Link>
         </li>
