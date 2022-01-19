9c79de7001779222c215fc05c9e5b6c87f6c6e30
ei-show <offiter.ei.show@gmail.com>
2022-01-19 22:18:39 +0900

    aspidaに移行したためclientを削除

--- src/libs/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "microcms-js-sdk"
-
-export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_ID ?? '0123456789abcdef',
-  apiKey: process.env.API_KEY ?? '0123456789abcdef',
-})
-
-export default client
