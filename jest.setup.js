738e9ea701e1fd309393759b27c649efb601bf7c
ei-show <offiter.ei.show@gmail.com>
2022-01-22 21:00:43 +0900

    setup jest

---
+++ jest.setup.js
@@ -0,0 +1,12 @@
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect'
+const fetchPolifill = require('whatwg-fetch')
+
+global.fetch = fetchPolifill.fetch
+global.Request = fetchPolifill.Request
+global.Headers = fetchPolifill.Headers
+global.Response = fetchPolifill.Response
