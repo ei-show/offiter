bcd0adc1456f620acb1baa3909ac45cd5ebc0908
ei-show <offiter.ei.show@gmail.com>
2026-03-07 18:46:54 +0900

    fix: tagslistを修正

--- src/components/TagsLists.tsx
+++ src/components/TagsLists.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Link from 'next/link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import type { tag } from '@/src/index'
 
 type TagsLists = {
@@ -15,23 +14,17 @@ const TagsLists = ({ tags }: TagsLists) => {
     <div className="mb-10 px-8">
       <h2 className="mb-4 font-head text-xl text-base-content">タグ</h2>
       <div className="card bg-base-100 shadow-sm lg:shadow-none">
-        <div className="card-body p-4">
-          <ul className="menu">
-            {tags.map((tag) => (
-              <React.Fragment key={tag.id}>
-                <li>
-                  <Link href="/pages/tags/[id]" as={`/pages/tags/${tag.id}`} className="group text-base-content">
-                    {tag.name}{' '}
-                    <FontAwesomeIcon
-                      icon={['fas', 'arrow-right']}
-                      className="transform transition duration-300 ease-in-out group-hover:translate-x-1"
-                    />
-                  </Link>
-                </li>
-              </React.Fragment>
-            ))}
-          </ul>
-        </div>
+        <ul className="card-body p-4 grid grid-cols-2">
+          {tags.map((tag) => (
+            <React.Fragment key={tag.id}>
+              <li>
+                <Link href="/pages/tags/[id]" as={`/pages/tags/${tag.id}`} className="btn btn-soft display-flex">
+                  {tag.name}{' '}
+                </Link>
+              </li>
+            </React.Fragment>
+          ))}
+        </ul>
       </div>
     </div>
   )
